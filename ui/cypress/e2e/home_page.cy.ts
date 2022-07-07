78f6b47fd06cb8dbec678db31ee1476233e1f3eb
s-horii <s2010310horii@gmail.com>
2022-07-07 21:49:21 +0900

    Commit leakage

--- ui/cypress/e2e/home_page.cy.ts
+++ ui/cypress/e2e/home_page.cy.ts
@@ -1,38 +1,36 @@
 describe('HomePage', () => {
     beforeEach(() => {
-        cy.exec('(cd ../databases/postgres && flyway clean && flyway migrate)');
-        cy.request('POST', '/api/talk/room', {
-            name: 'room_name1',
-            description: 'room_description1',
+        cy.exec("(cd ../databases/postgres && flyway clean && flyway migrate)");
+        cy.request("POST", "/api/talk/room", {
+            name: "room_name1",
+            description: "room_description1"
         });
-        cy.request('POST', '/api/talk/room', {
-            name: 'room_name2',
-            description: 'room_description2',
+        cy.request("POST", "/api/talk/room", {
+            name: "room_name2",
+            description: "room_description2"
         });
         cy.visit('/');
-    });
+    })
     it('test load', () => {
-        cy.contains('room_name1');
-        cy.contains('room_description1');
-        cy.contains('room_name2');
-        cy.contains('room_description2');
+        cy.contains("room_name1");
+        cy.contains("room_description1");
+        cy.contains("room_name2");
+        cy.contains("room_description2");
     });
     it('test press create button', () => {
         cy.get('[data-cy="HomeContainer-create"]').click();
-        cy.url().should('eq', `${Cypress.config().baseUrl}/room/create`);
+        cy.url().should("eq", `${Cypress.config().baseUrl}/room/create`)
     });
     it('test press edit button', () => {
         cy.get('[data-cy="HomeContainer-menu-0"]').click();
         cy.get('[data-cy="HomeContainer-edit"]').click();
-        cy.url().should('match', /room\/.*\/update/);
-        cy.get('[data-cy="RoomForm-name"] input').should('have.value', 'room_name1');
-        cy.get('[data-cy="RoomForm-description"] input').should('have.value', 'room_description1');
+        cy.url().should("match", /room\/.*\/update/);
     });
     it('test press delete button', () => {
         cy.get('[data-cy="HomeContainer-menu-0"]').click();
         cy.get('[data-cy="HomeContainer-delete"]').click();
-        cy.url().should('eq', `${Cypress.config().baseUrl}/`);
-        cy.contains('room_name1').should('not.exist');
-        cy.contains('room_description1').should('not.exist');
+        cy.url().should("eq", `${Cypress.config().baseUrl}/`);
+        cy.contains("room_name1").should("not.exist");
+        cy.contains("room_description1").should("not.exist");
     });
 });
